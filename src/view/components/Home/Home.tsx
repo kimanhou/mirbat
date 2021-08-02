cea17133c4db372a826db4b2e690cf48b41ccf5a
Kim Anh Nguyen <kimanhou@gmail.com>
2021-08-02 16:06:47 +0300

    Home hero : dezoom on scroll

--- src/view/components/Home/Home.tsx
+++ src/view/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProjectCategory } from '../../../model/Project';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -16,6 +16,19 @@ const Home : React.FC<IHomeProps> = props => {
     const [ isNavVisible, setIsNavVisible ] = useState(false);
     const isNavVisibleClassName = isNavVisible ? 'nav-visible' : '';
 
+    const [ backgroundSize, setBackgroundSize ] = useState('120%');
+
+    useEffect(() => {
+        const onScroll = () => {
+            const scrollMax = 20 + 0.5 * document.documentElement.clientHeight;
+            const backgroundSize = - (window.scrollY) / scrollMax * 20 + 120;
+            const limitedBackgroundSize = Math.max(Math.min(backgroundSize, 120), 100);
+            setBackgroundSize(`${limitedBackgroundSize}%`);
+        };
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [ ]);
+
     return (
         <div className={`home`}>
             <Header isNavVisible={isNavVisible} setIsNavVisible={setIsNavVisible} />
@@ -23,7 +36,7 @@ const Home : React.FC<IHomeProps> = props => {
             <div className={`home-content`}>
                 <div className={`home-content-moving ${isNavVisibleClassName}`}>
                     <Header isNavVisible={isNavVisible} setIsNavVisible={setIsNavVisible} iswhite/>
-                    <div className={`home-hero`}></div>
+                    <div className={`home-hero`} style={{ backgroundSize: backgroundSize }}></div>
                     <div className={`home-tag-line`}>
                         Lorem ipsum dolor sit amet, <br></br>consectetur adipiscing elit. 
                     </div>
